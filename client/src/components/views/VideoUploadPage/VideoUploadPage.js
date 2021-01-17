5c577e11296367ebd7ae6f62747aef439d3c2208
seojin <lijvlij@naver.com>
2021-01-17 17:23:41 +0900

    first commit

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// rfce 를 입력하면 자동으로 기본코드가 입력된다.
+
+function VideoUploadPage() {
+    return (
+        <div>
+            wow
+        </div>
+    )
+}
+
+export default VideoUploadPage
